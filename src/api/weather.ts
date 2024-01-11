204265cbacd27634abb6c7144f4068bf507f4a1b
Bartłomiej B <bartlomiejb@MacBook-Pro-Bartomiej.local>
2024-01-11 19:15:40 +0100

    Connect to API

---
+++ src/api/weather.ts
@@ -0,0 +1,26 @@
+import { API_KEY, BACKEND_URL } from "@env";
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: BACKEND_URL,
+  timeout: 10000,
+  headers: {
+    "x-api-key": API_KEY,
+    Accept: "application/json",
+  },
+});
+
+const apiCall = async (endpoint: string) => {
+  try {
+    const response = await instance.get(endpoint);
+    return response.data;
+  } catch (error) {
+    console.error("error: ", JSON.stringify(error));
+  }
+};
+
+export const fetchWeather = (params: { lat: string; lon: string }) =>
+  apiCall(`/weather/forecast?lat=${params.lat}&lon=${params.lon}`);
+
+export const fetchLocations = (params: { cityName: string }) =>
+  apiCall(`/weather/location-list?location=${params.cityName}`);
